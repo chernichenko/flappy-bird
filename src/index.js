87b89f7a61c322beffc510d096b27257a089095d
Che <alexchernichenko18@gmail.com>
2021-04-06 19:33:31 +0300

    Finish

--- src/index.js
+++ src/index.js
@@ -1,17 +1,30 @@
 import './styles/app.scss'
 
-const cvs = document.getElementById("canvas")
+const title = document.getElementById("title")
 const button = document.getElementById("start")
+const score = document.getElementById("score")
+const cvs = document.getElementById("canvas")
 const ctx = cvs.getContext("2d")
 
 let isCanStart = true
 let isFinished = false
+const gravitaion = 0.9
+const blockWidth = 50
+const holeHeight = 150
+let scoreCount = 0
 
-const xPos = 30
-let yPos = 235
-const birdWidth = 30
-const birdHeight = 30
-const gravitaion = 0.7 
+const bird = {
+    xPos: 30,
+    yPos: 235,
+    width: 30,
+    height: 30,
+}
+
+const blocks = [{
+    x: cvs.width,
+    y: 0,
+    topItemHeight: 150,
+}]
 
 const firstRender = () => {
     ctx.fillStyle = "white";
@@ -20,23 +33,54 @@ const firstRender = () => {
 }
 firstRender()
 
-const moveUp = () => yPos -= 25
+const moveUp = () => bird.yPos -= 30
 document.addEventListener("keydown", moveUp)
 
-const renderBird = () => {
+const renderBlock = (x, y, width, height) => {
     ctx.beginPath()
-    ctx.rect(xPos, yPos, birdWidth, birdHeight)
+    ctx.rect(x, y, width, height)
     ctx.fillStyle = 'white'
     ctx.fill()
 }
 
+const renderBird = () => renderBlock(bird.xPos, bird.yPos, bird.width, bird.height)
+
 const render = () => {
     ctx.clearRect(0, 0, cvs.width, cvs.height)
-    yPos += gravitaion
+    bird.yPos += gravitaion
 
     renderBird()
-    
-    if (yPos + birdHeight < cvs.height) {
+
+    blocks.forEach(block => {
+        renderBlock(block.x, block.y, blockWidth, block.topItemHeight)
+        renderBlock(block.x, block.topItemHeight + holeHeight, blockWidth, cvs.height - (block.topItemHeight + holeHeight))
+
+        block.x = block.x - 2
+
+        if (bird.xPos + bird.width >= block.x && bird.xPos <= block.x + blockWidth) {
+            const maxCurrentTop = block.y + block.topItemHeight
+            const maxCurrentBottom = maxCurrentTop + holeHeight
+            if (bird.yPos < maxCurrentTop || bird.yPos + bird.height > maxCurrentBottom) {
+                isFinished = true
+                return
+            }
+        }
+
+        if (block.x === 200) {
+            blocks.push({
+                x: cvs.width,
+                y: 0,
+                topItemHeight: 100 + Math.floor(Math.random() * 200),
+            })
+        }
+
+        if (block.x === 10) {
+            scoreCount++
+            score.innerHTML = scoreCount
+        }
+    })
+
+    if (bird.yPos + bird.height < cvs.height && !isFinished) {
         requestAnimationFrame(render)
     } else {
         isFinished = true
@@ -50,7 +94,7 @@ const onStartHandler = () => {
         render()
     }
     if (isFinished) {
-        location.reload()
+        title.innerHTML = `Fail. Score: ${scoreCount}`
     }
 }
 
